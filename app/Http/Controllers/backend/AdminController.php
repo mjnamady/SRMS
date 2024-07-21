9b263abdd498b4f7f3f32b2ceafb13b3000b455d
mjnamady <mustaphajnamadi@gmail.com>
2024-07-21 03:41:25 +0100

    Admin Profile Change Password Part 2

--- app/Http/Controllers/backend/AdminController.php
+++ app/Http/Controllers/backend/AdminController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers\backend;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
-use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
@@ -53,5 +54,32 @@ class AdminController extends Controller
 
     public function AdminPasswordChange(){
         return view('admin.admin_password_change');
+    } // End method
+
+    public function AdminPasswordUpdate(Request $request){
+        $request->validate([
+            'old_password' => 'required',
+            'new_password' => 'required|confirmed'
+        ]);
+
+        if(!Hash::check($request->old_password, Auth::user()->password)){
+            $notification = array(
+                'message' => 'Old Password Does Not Match!',
+                'alert-type' => 'error'
+            );
+    
+            return redirect()->back()->with($notification);
+        }
+
+        User::whereId(Auth::user()->id)->update([
+            'password' => Hash::make($request->new_password)
+        ]);
+
+        $notification = array(
+            'message' => 'Password Updated Successfully!',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
     } // End method
 }
--- resources/views/admin/admin_password_change.blade.php
+++ resources/views/admin/admin_password_change.blade.php
@@ -26,7 +26,7 @@
         <div class="card-body">
 
             <h4 class="card-title">ADMIN'S PASSWORD - Update</h4>
-        <form action="{{route('admin.profile.update')}}" method="POST">
+        <form action="{{route('admin.password.update')}}" method="POST">
             @csrf
             <div class="row mb-3">
                 <label for="example-text-input" class="col-sm-2 col-form-label">Old Password</label>
