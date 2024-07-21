c449ab7019bc170f77e9cd43597431d211317c94
mjnamady <mustaphajnamadi@gmail.com>
2024-07-21 23:34:01 +0100

    22-Admin Profile Image Updateds

--- resources/views/admin/sections/header.blade.php
+++ resources/views/admin/sections/header.blade.php
@@ -144,12 +144,16 @@
                 </div>
             </div>
 
+            @php
+                $adminData = App\Models\User::findOrFail(Auth::user()->id);
+            @endphp
+
             <div class="dropdown d-inline-block user-dropdown">
                 <button type="button" class="btn header-item waves-effect" id="page-header-user-dropdown"
                     data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <img class="rounded-circle header-profile-user" src="{{asset('backend/assets/images/users/avatar-1.jpg')}}"
+                    <img class="rounded-circle header-profile-user" src="{{ empty($adminData->photo)? asset('uploads/no_image.png') : asset('uploads/admin_profiles/'.$adminData->photo) }}"
                         alt="Header Avatar">
-                    <span class="d-none d-xl-inline-block ms-1">Julia</span>
+                    <span class="d-none d-xl-inline-block ms-1">{{$adminData->name}}</span>
                     <i class="mdi mdi-chevron-down d-none d-xl-inline-block"></i>
                 </button>
                 <div class="dropdown-menu dropdown-menu-end">
